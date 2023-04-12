42ce82442d0d47340122ad4cec353050aeb2e89a
luah5 <raymondvleeshouwer@gmail.com>
2023-04-12 15:25:51 +0100

    Add stb_loader.c

---
+++ stb_loader.c
@@ -0,0 +1,10 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// TODO: Add functions for loading images with python .so
+int main(int argc, char const *argv[])
+{
+    printf("Hello, world!\n");
+
+    return 0;
+}
